89faa98156a91cf0d5d1c91f20f257bac96736e0
Calvin Christian <calvinchrst@gmail.com>
2020-01-21 11:12:55 +0700

    Save Products to a local JSON file

--- controllers/products.js
+++ controllers/products.js
@@ -16,13 +16,14 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        docTitle: 'Shop',
-        path: "/",
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    products = Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products,
+            docTitle: 'Shop',
+            path: "/",
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        }); 
     });
 }
--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const path = require('path');
+const fs = require('fs');
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +7,39 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            
+            // Read and parse existing products from file
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+
+            // Add this new product
+            products.push(this);
+
+            // Save all products back to the file
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const p = path.join(path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+            // Read and parse existing products from file
+            if (err) {
+                return cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
 }
