ca2bc88d4e1746d483849f1b6990a9f6839c3afc
Calvin Christian <calvinchrst@gmail.com>
2020-02-08 17:38:10 +0800

    Add User Model & Make Product Creation based on User Logged In

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize
-    .sync()
-    .then( result => {
-        app.listen(3000);
-    })
-    .catch( err => {
-        console.log(err);
-    });
+// Set Sequelize / Database Table association
+Product.belongsTo(User, { constraints: true, onDelete: "cascade" });
+User.hasMany(Product);
 
-console.log('Server startup Done');
+// Initialize Database & Start Listening
+sequelize
+  .sync()
+  // .sync({ force: true })
+  .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    // If Dummy User with Id 1 does not exist, create it & use it. Otherwise just use existing user with id 1.
+    if (!user) {
+      return User.create({ name: "Max", email: "test@gmail.com" });
+    }
+    return User; // By right should be return Promise.resolve(user)
+    // By right, This is inconsistent return because if the user is null we return a promise and if not, we return an object.
+    // However, because you return a value in a then block, it is automatically wrap into a new promise
+  })
+  .then(user => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+console.log("Server startup Done");
--- controllers/admin.js
+++ controllers/admin.js
@@ -14,12 +14,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    imageUrl: imageUrl,
-    price: price,
-    description: description
-  })
+  req.user
+    .createProduct({
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      description: description
+    })
     .then(() => {
       res.redirect("/admin/products");
       console.log("Product Added");
