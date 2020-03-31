e3819794bba9899a77de06a58abaad77613ce2e7
Calvin Christian <calvinchrst@gmail.com>
2020-03-31 13:39:41 +0800

    Make get orders functionality works with Mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -87,18 +87,17 @@ exports.postCartDeleteItem = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-// exports.getOrders = (req, res, next) => {
-//   req.user
-//     .getOrders()
-//     .then(orders =>
-//       res.render("shop/orders", {
-//         path: "/orders",
-//         pageTitle: "Your Orders",
-//         orders: orders
-//       })
-//     )
-//     .catch(err => console.log(err));
-// };
+exports.getOrders = (req, res, next) => {
+  Order.find({ "user.userId": req.user._id })
+    .then(orders =>
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders
+      })
+    )
+    .catch(err => console.log(err));
+};
 
 exports.postCreateOrder = (req, res, next) => {
   req.user
