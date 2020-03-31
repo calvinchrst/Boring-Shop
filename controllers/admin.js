deea20c81376bbcd5635bd4e70baac3b3eb0823d
Calvin Christian <calvinchrst@gmail.com>
2020-03-31 09:06:07 +0800

    Make Edit Product functionality works with Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,18 +1,18 @@
 const Product = require("../models/product");
 
-// exports.getProducts = (req, res, next) => {
-//   Product.fetchAll()
-//     .then(products => {
-//       res.render("admin/products", {
-//         prods: products,
-//         pageTitle: "Admin Products",
-//         path: "/admin/products"
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
@@ -45,51 +45,49 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect("/");
-//   }
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
 
-//   const prodId = req.params.productId;
-//   Product.fetchProductById(prodId)
-//     .then(product => {
-//       res.render("admin/edit-product", {
-//         pageTitle: "Edit Product",
-//         path: "/admin/edit-product",
-//         product: product,
-//         editMode: editMode
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(product => {
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        product: product,
+        editMode: editMode
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-// exports.postEditProduct = (req, res, next) => {
-//   productId = req.body.productId;
-//   updatedTitle = req.body.title;
-//   updatedImageUrl = req.body.imageUrl;
-//   updatedDesc = req.body.description;
-//   updatedPrice = req.body.price;
-//   product = new Product(
-//     updatedTitle,
-//     updatedPrice,
-//     updatedDesc,
-//     updatedImageUrl,
-//     productId
-//   );
-
-//   product
-//     .save()
-//     .then(result => {
-//       console.log(result);
-//       res.redirect("/admin/products");
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
+exports.postEditProduct = (req, res, next) => {
+  productId = req.body.productId;
+  updatedTitle = req.body.title;
+  updatedImageUrl = req.body.imageUrl;
+  updatedDesc = req.body.description;
+  updatedPrice = req.body.price;
+  Product.findById(productId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then(result => {
+      console.log("UPDATED PRODUCT!");
+      res.redirect("/admin/products");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   productId = req.body.productId;
