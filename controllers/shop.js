8d147f0f04ddce47d5b1d638cd6760aaede4b2a6
Calvin Christian <calvinchrst@gmail.com>
2020-03-31 08:57:47 +0800

    Make get product and get index works with Mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,18 +1,18 @@
 const Product = require("../models/product");
 
-// exports.getProducts = (req, res, next) => {
-//   Product.fetchAll()
-//     .then(products => {
-//       res.render("shop/product-list", {
-//         prods: products,
-//         pageTitle: "All Products",
-//         path: "/products"
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 // exports.getProduct = (req, res, next) => {
 //   const prodId = req.params.productId;
@@ -32,19 +32,19 @@ const Product = require("../models/product");
 //     });
 // };
 
-// exports.getIndex = (req, res, next) => {
-//   Product.fetchAll()
-//     .then(products => {
-//       res.render("shop/index", {
-//         prods: products,
-//         pageTitle: "Shop",
-//         path: "/"
-//       });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 // exports.getCart = (req, res, next) => {
 //   req.user
