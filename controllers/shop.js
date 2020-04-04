418c3db93151bafa193b52ac32a5f8824d04d25a
Calvin Christian <calvinchrst@gmail.com>
2020-04-04 17:30:32 +0800

    Make Page Links dynamic depending on the number of products available and the current page number

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,7 +6,7 @@ const PDFDocument = require("pdfkit");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
-const NR_PROD_PER_PAGE = 2;
+const NR_PROD_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -43,14 +43,27 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
   Product.find()
-    .skip((req.query.page - 1) * NR_PROD_PER_PAGE)
-    .limit(NR_PROD_PER_PAGE)
+    .count()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * NR_PROD_PER_PAGE)
+        .limit(NR_PROD_PER_PAGE);
+    })
     .then(products => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
-        path: "/"
+        path: "/",
+        currentPage: page,
+        hasNextPage: page * NR_PROD_PER_PAGE < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / NR_PROD_PER_PAGE)
       });
     })
     .catch(err => {
